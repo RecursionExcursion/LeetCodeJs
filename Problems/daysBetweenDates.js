7cfe346ac123a56c18cc0e25ea6f6c82523ecd58
RecursionExcursion <rloup15@gmail.com>
2023-09-16 13:28:20 -0400

    probs

--- Problems/daysBetweenDates.js
+++ Problems/daysBetweenDates.js
@@ -1,6 +1,5 @@
 var daysBetweenDates = function (date1, date2) {
     return Math.abs((new Date(date1) - new Date(date2)) / (1000 * 60 * 60 * 24))
-
 };
 
 console.log(daysBetweenDates("2019-06-29", "2019-06-30"))
